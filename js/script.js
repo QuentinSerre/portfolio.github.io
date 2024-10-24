469d7139d402e935e3131c09cace1b5cea6928fc
QuentinSerre <quentin.serre@orange.fr>
2024-10-24 12:29:42 +0200

    ajout sous-menu dans la rubrique profil

--- js/script.js
+++ js/script.js
@@ -12,30 +12,3 @@ function openCloseNav(){
         menu.style.transform = 'translateX(0%)';
     }
 }
-
-// Open, close sub menu
-// function openCloseSubMenu(){
-//     var subMenu = document.getElementById("sub_menu");
-
-//     // navigation menu is close
-//     if (subMenu.style.transform == 'translateY(0%)'){
-//         // Open navigation menu
-//         subMenu.style.transform = 'translateY(100%)';
-//     } else {
-//         // Close navigation menu
-//         subMenu.style.transform = 'translateY(0%)';
-//     }
-// }
-
-function openCloseSubMenu(){
-    var subMenu = document.getElementById("sub_menu");
-
-    // navigation menu is close
-    if (subMenu.style.display == 'none'){
-        // Open navigation menu
-        subMenu.style.display = 'block';
-    } else {
-        // Close navigation menu
-        subMenu.style.display = 'none';
-    }
-}
