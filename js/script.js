c107a288df5007f0ed286b348b1fb047e739d59d
QuentinSerre <quentin.serre@orange.fr>
2024-11-29 11:36:29 +0100

    ajout visuel production_card

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function openCloseNav(){
 
     var menu = document.getElementById("nav_menu");
 
-    // navigation menu is close
+    // navigation menu is closed
     if (menu.style.transform == 'translateX(0%)'){
         // Open navigation menu
         menu.style.transform = 'translateX(100%)';
