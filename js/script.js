4980794d88afb9f6ef0afb0273a3fda53390717d
QuentinSerre <quentin.serre@orange.fr>
2025-02-14 08:43:33 +0100

    resolution probleme clic menu de navigation

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function openCloseNav(){
     var menu = document.getElementById("nav_menu");
 
     // navigation menu is closed
-    if (menu.style.transform == 'translateX(0%)'){
+    if (menu.style.transform == 'translateX(0%)' | menu.style.transform == ""){
         // Open navigation menu
         menu.style.transform = 'translateX(100%)';
     } else {
